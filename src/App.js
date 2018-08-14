7cc7dbfd4ce8d413e6dca01567edb8d252fc7291
TS22082 <ts22082@gmail.com>
2018-08-14 16:42:42 -0700

    added newsapi.org credits and github link to footer

--- src/App.js
+++ src/App.js
@@ -91,14 +91,38 @@ class App extends Component {
       backgroundColor: "dimgray",
       paddingLeft: "10px"
     };
+    var footerStyle = {
+      color: "white",
+      display: "grid",
+      gridTemplateColumns: "1fr",
+      alignItems: "center",
+      backgroundColor: "black"
+    };
+    var newsApiStyle = {
+      textDecoration: "none",
+      color: "#2ecc71"
+    };
+    var gitHubStyle = {
+      textDecoration: "none",
+      color: "#3498db"
+    };
     return (
       <div className="App">
         <div style={headerStyle}>
           <p>The Your_News Project</p>
         </div>
         {this.state.rows}
-        <div className="footer">
-          <p>Powered by newapi.org </p>
+        <div style={footerStyle}>
+          <p style={{ textAlign: "center" }}>
+            Powered by{" "}
+            <a style={newsApiStyle} href="https://newsapi.org/">
+              newsapi.org
+            </a>{" "}
+            and{" "}
+            <a style={gitHubStyle} href="https://github.com/TS22082/your_news">
+              open sourced
+            </a>
+          </p>
         </div>
       </div>
     );
