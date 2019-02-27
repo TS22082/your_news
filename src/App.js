7105e91f87d5df848cea6f8fbcae75bd42313f48
GeekCentric <ts22082@gmail.com>
2019-02-27 10:02:48 -0800

    spring cleaning

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 import NewsFeed from './Components/NewsFeed/NewsFeed'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
   render() {
-    var headerStyle = {
-      color: 'white',
-      fontSize: '1.25em',
-      backgroundColor: 'dimgray',
-      paddingLeft: '10px'
-    }
-    var footerStyle = {
-      marginTop: '10px',
-      color: 'white',
-      display: 'grid',
-      gridTemplateColumns: '1fr',
-      alignItems: 'center',
-      backgroundColor: 'black'
-    }
-    var newsApiStyle = {
-      textDecoration: 'none',
-      color: '#2ecc71'
-    }
-    var gitHubStyle = {
-      textDecoration: 'none',
-      color: '#3498db'
-    }
     return (
       <div className="App">
-        <div style={headerStyle}>
-          <p>The Your_News Project</p>
-        </div>
+        <Header />
         <NewsFeed />
-        <div style={footerStyle}>
-          <p style={{ textAlign: 'center' }}>
-            Powered by{' '}
-            <a style={newsApiStyle} href="https://newsapi.org/">
-              newsapi.org
-            </a>{' '}
-            and{' '}
-            <a style={gitHubStyle} href="https://github.com/TS22082/your_news">
-              open sourced
-            </a>
-          </p>
-        </div>
+        <Footer />
       </div>
     )
   }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <p>Aggregate News</p>
+      </div>
+    )
+  }
+}
+
+export default Header
