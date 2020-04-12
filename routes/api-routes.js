c069438189a851b4e93121364d8c52e82920754e
TS22082 <ts22082@gmail.com>
2020-04-11 22:14:06 -0700

    reformat route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,10 +15,16 @@ router.get("/news/top-news", (req, res) => {
 });
 
 router.get("/weather/by-location", async (req, res) => {
+  const weatherUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    req.query.lat +
+    "&lon=" +
+    req.query.lon +
+    "&units=imperial&APPID=" +
+    weather_key;
+
   try {
-    const weatherResults = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&units=imperial&APPID=${weather_key}`
-    );
+    const weatherResults = await axios.get(weatherUrl);
     res.send(weatherResults.data);
   } catch {
     res.send({ message: "There was an error" });
