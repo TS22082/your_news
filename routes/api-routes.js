3cc50c19802f04699968552dee69eea19e2c20f7
TS22082 <ts22082@gmail.com>
2020-04-11 19:58:23 -0700

    fix route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,9 +8,7 @@ const news_key = process.env.NEWS_API;
 
 router.get("/news/top-news", (req, res) => {
   axios
-    .get(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=84145fe9236e4573824511f495ab8126"
-    )
+    .get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${news_key}`)
     .then((newsResponse) => res.send(newsResponse.data))
     .catch(() => res.send({ message: "error no articles retrieved" }));
 });
