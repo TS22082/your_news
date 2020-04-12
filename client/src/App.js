18c41f49fff3586f348d058f9e18bc413f9cfac9
TS22082 <ts22082@gmail.com>
2020-04-12 14:14:43 -0700

    fix css sizing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import NewsFeed from "./Components/NewsFeed/NewsFeed";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <NewsFeed />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header />
+      <NewsFeed />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
