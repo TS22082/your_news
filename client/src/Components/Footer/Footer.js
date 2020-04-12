3a5917032365bf2888f4f4265037a734460ae230
TS22082 <ts22082@gmail.com>
2020-04-12 11:16:04 -0700

    refactor css

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import './footer.css'
+import React, { Component } from "react";
+import "./footer.css";
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
         <p>
-          Powered by{' '}
+          Powered by{" "}
           <a className="newsApi" href="https://newsapi.org/">
             newsapi.org
-          </a>{' '}
-          and{' '}
+          </a>{" "}
+          and{" "}
           <a className="github" href="https://github.com/TS22082/your_news">
             open sourced
           </a>
         </p>
       </div>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
