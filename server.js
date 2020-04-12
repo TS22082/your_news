44092f182684331047f2de66448bcc9087557345
TS22082 <ts22082@gmail.com>
2020-04-12 14:50:10 -0700

    re-arange server file

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const apiRoutes = require("./routes/api-routes");
+app.use("/", apiRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const apiRoutes = require("./routes/api-routes");
-app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`listening at: http://localhost:${PORT}`);
