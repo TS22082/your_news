1da9b26971298cd064f339e5e532cba27aa61719
TS22082 <ts22082@gmail.com>
2020-05-28 15:31:35 -0700

    declutter and deconstruct data for readability

--- controllers/index.js
+++ controllers/index.js
@@ -6,19 +6,18 @@ const news_key = process.env.NEWS_API;
 const getWeather = async (req, res) => {
   try {
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&units=imperial&APPID=${weather_key}`;
-    const weatherResults = await axios.get(weatherUrl);
-    res.send(weatherResults.data);
+    const { data } = await axios.get(weatherUrl);
+    res.send(data);
   } catch (err) {
     res.send({ errror: err });
   }
 };
 
 const getNews = async (req, res) => {
-  const newsUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${news_key}`;
-
   try {
-    const newsResults = await axios.get(newsUrl);
-    res.send(newsResults.data);
+    const newsUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${news_key}`;
+    const { data } = await axios.get(newsUrl);
+    res.send(data);
   } catch (err) {
     res.send({ error: err });
   }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
 const { getWeather, getNews } = require("../controllers/");
-
-const axios = require("axios");
-require("dotenv").config();
 
 router.route("/news/top-news").get(getNews);
 router.route("/weather/by-location").get(getWeather);
