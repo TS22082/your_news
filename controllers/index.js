f4d3616a157bf8fc5d880b5580fce5fd68452f63
TS22082 <ts22082@gmail.com>
2020-11-15 09:55:36 -0800

    update modules

--- controllers/index.js
+++ controllers/index.js
@@ -7,9 +7,9 @@ const getWeather = async (req, res) => {
   try {
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&units=imperial&APPID=${weather_key}`;
     const { data } = await axios.get(weatherUrl);
-    res.send(data);
+    res.json(data);
   } catch (err) {
-    res.send({ errror: err });
+    res.send({ error: err });
   }
 };
 
