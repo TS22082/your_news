7feff2b247e9eb4317882168c9e9213e4d4a3acb
TS22082 <ts22082@gmail.com>
2021-03-29 15:50:48 -0700

    add to style

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,22 +4,25 @@ import "./footer.css";
 
 function Footer() {
   return (
-    <div className="footer">
-      <p>
-        Powered by{" "}
-        <a className="newsApi" href="https://newsapi.org/">
-          newsapi.org
-        </a>
-        {",  "}
-        <a className="openWeather" href="https://openweathermap.org/">
-          open weather map
-        </a>{" "}
-        and{" "}
-        <a className="github" href="https://github.com/TS22082/your_news">
-          open sourced
-        </a>
-      </p>
-    </div>
+    <>
+      <div className="hr" />
+      <div className="footer">
+        <p>
+          Powered by{" "}
+          <a className="newsApi" href="https://newsapi.org/">
+            newsapi.org
+          </a>
+          {",  "}
+          <a className="openWeather" href="https://openweathermap.org/">
+            open weather map
+          </a>{" "}
+          and{" "}
+          <a className="github" href="https://github.com/TS22082/your_news">
+            open sourced
+          </a>
+        </p>
+      </div>
+    </>
   );
 }
 
--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -4,10 +4,13 @@ import WeatherPreview from "./weather_preview/WeatherPreview";
 
 function Header() {
   return (
-    <div className="header">
-      <h3 className="title">Aggregate News</h3>
-      <WeatherPreview />
-    </div>
+    <>
+      <div className="header">
+        <h3 className="title">Aggregate News</h3>
+        <WeatherPreview />
+      </div>
+      <div className="hr" />
+    </>
   );
 }
 
